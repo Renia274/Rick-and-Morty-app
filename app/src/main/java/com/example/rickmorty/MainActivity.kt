b83a1e90fb59f4856df04f14238c0e3a9a306992
Dioleti Renia <reniadiol3@gmail.com>
2023-03-30 23:30:18 +0300

    display characters in cardview

--- app/src/main/java/com/example/rickmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickmorty/MainActivity.kt
@@ -1,29 +1,27 @@
 package com.example.rickmorty
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: SharedViewModel by lazy {
-        ViewModelProvider(this).get(SharedViewModel::class.java)
-    }
-
+    private lateinit var viewModel: SharedViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+
+        // Call refreshCharacter() method with the character id you want to display
+        viewModel.refreshCharacter(1)
 
         val nameTextView = findViewById<TextView>(R.id.nameTextView)
         val headerImageView = findViewById<ImageView>(R.id.headerImageView)
@@ -32,25 +30,34 @@ class MainActivity : AppCompatActivity() {
         val speciesTextView = findViewById<TextView>(R.id.SpeciesTextView)
         val genderImageView = findViewById<ImageView>(R.id.genderImageView)
 
-        viewModel.characterLiveData.observe(this){response->
-            if(response==null){
-
-                Toast.makeText(this@MainActivity,"not success call to the network",Toast.LENGTH_SHORT).show()
+        viewModel.characterLiveData.observe(this) { response ->
+            if (response == null) {
+                Toast.makeText(this@MainActivity, "Not able to fetch the data", Toast.LENGTH_SHORT).show()
                 return@observe
-        }
+            }
 
-            nameTextView.text = response?.name
-            aliveTextView.text = response?.status
-            speciesTextView.text = response?.species
-            originTextView.text = response?.origin?.name
+            // Update the UI views with the character data
+            nameTextView.text = response.name
+            aliveTextView.text = response.status
+            speciesTextView.text = response.species
+            originTextView.text = response.origin?.name
 
-            if(response?.gender.equals("male", ignoreCase = true)){
-            genderImageView.setImageResource(R.drawable.ic_male_24)
+            // Set visibility of views to make sure they are visible
+            nameTextView.visibility = View.VISIBLE
+            aliveTextView.visibility = View.VISIBLE
+            speciesTextView.visibility = View.VISIBLE
+            originTextView.visibility = View.VISIBLE
+            genderImageView.visibility = View.VISIBLE
+            headerImageView.visibility = View.VISIBLE
+
+            if (response.gender.equals("male", ignoreCase = true)) {
+                genderImageView.setImageResource(R.drawable.ic_male_24)
             } else {
-                    genderImageView.setImageResource(R.drawable.ic_female_24)
-               }
+                genderImageView.setImageResource(R.drawable.ic_female_24)
+            }
 
-            Picasso.get().load(response?.image).into(headerImageView)
+            Picasso.get().load(response.image).into(headerImageView)
 
+        }
     }
-}}
+}
