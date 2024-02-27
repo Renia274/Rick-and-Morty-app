0ddd782c7f2d1596e9b2950e37fa4e80dcfd4e02
Dioleti Renia <reniadiol3@gmail.com>
2024-02-27 18:45:27 +0200

    change the viewModel

--- app/src/main/java/com/example/rickmorty/MyViewModel.kt
+++ app/src/main/java/com/example/rickmorty/MyViewModel.kt
@@ -6,25 +6,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class MyViewModel:ViewModel(){
+class MyViewModel: ViewModel() {
 
     // Creation of a SharedRepository object to get data from the data source
     private val repository = SharedRepository()
-    //live data for network response
-    private val _characterByIdLiveData=MutableLiveData<ApiCharacterResponse?>()
-    //regular live data
-    val characterLiveData:LiveData<ApiCharacterResponse?> =_characterByIdLiveData
+    // Live data for network response
+    private val characterByIdLiveData = MutableLiveData<ApiCharacterResponse?>()
+    // Regular live data
+    val characterLiveData: LiveData<ApiCharacterResponse?> = characterByIdLiveData
 
-    // This function refreshes the character data for a given character ID
-    fun refreshCharacter(characterid:Int){
+    // refreshes the character data for a given character ID
+    fun refreshCharacter(characterid: Int) {
         viewModelScope.launch {
 
             // Get the character data for the given ID from the repository
-            val response=repository.getCharacterById(characterid)
+            val response = repository.getCharacterById(characterid)
 
-            //post response to the live data object
-            _characterByIdLiveData.postValue(response)
+            // Post response to the live data object
+            characterByIdLiveData.postValue(response)
         }
     }
-
 }
